d3c2a2f5857aa289b5305bc47bcc90cf9b3330fa
醉梦 <2272909403@qq.com>
2026-05-06 08:27:06 +0800

    加入Happyday.vue

---
+++ components/Happyday.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="happyday">恭喜你找到了彩蛋了喵，祝你今天愉快！</div>
+</template>
+
+<script>
+export default {
+  name: "Happyday",
+}
+
+</script>
+
+<style scoped>
+.happyday {
+  font-size: 0rem!important;
+  opacity: 0!important;      
+  user-select: text!important; 
+  position: absolute!important;
+  width: 0rem!important;
+  height: 0rem!important;
+}
+</style>
